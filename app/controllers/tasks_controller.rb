9c95cda5e9b30fbdc6052201d0739a7e5bef0b90
satomi.tsutsui <stmtti4062@yahoo.co.jp>
2019-10-15 13:25:10 +0000

    task_controller mod

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,10 +46,12 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
 end
 
-private
-
-def task_params
-  params.require(:task).permit(:content, :status)
-end
